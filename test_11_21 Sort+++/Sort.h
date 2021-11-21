865ce6f477fdbaa34e4c96e8cccbf1cdde856eff
waywtl <1148117782@qq.com>
2021-11-21 15:21:42 +0800

    Sort+++

---
+++ test_11_21 Sort+++/Sort.h
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Stack.h"
+
+void PrintArray(int* a, int n);
+
+void InsertSort(int* a, int n);
+void ShellSort(int* a, int n);
+
+void SelectSort(int* a, int n);
+void HeapSort(int* a, int n);
+
+void BubbleSort(int* a, int n);
+void QuickSort(int* a, int left, int right);
+void QuickSortNonR(int* a, int left, int right);
+
+void MergeSort(int* a, int n);
+void MergeSortNonR(int* a, int n);
